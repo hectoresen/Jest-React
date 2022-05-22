33273d0ebf8a7c27c17b1450fbfdf1dd3394da6e
Hector <89035945+hectoresen@users.noreply.github.com>
2022-05-22 18:12:19 +0200

    FEAT: First section

---
+++ Jest Basics/src/__test__/global.test.js
@@ -0,0 +1,56 @@
+const text = "Hola Mundo";
+const fruits = ['manzana', 'melon', 'banana'];
+
+test('Debe contener un texo', () =>{
+    expect(text).toMatch(/Mundo/);
+});
+
+test('¿Tenemos una banana?', () =>{
+    expect(fruits).toContain('banana');
+});
+
+test('Mayor que', () =>{
+    expect(10).toBeGreaterThan(9);
+});
+
+test('Verdadero', () =>{
+    expect(true).toBeTruthy();
+});
+
+const reverseString = (str, callback) =>{
+    callback(str.split('').reverse().join(''))
+};
+
+test('Probar un callback', () =>{
+    reverseString('Hola', (str) =>{
+        expect(str).toBe('aloH')
+    })
+});
+
+
+const reverseString2 = str =>{
+    return new Promise((resolve, reject) =>{
+        if(!str){
+            reject(Error('Error, no hay String'))
+        }
+        resolve(str.split('').reverse().join(''))
+    });
+};
+
+test('Probar una promesa', () =>{
+    return reverseString2('Hola')
+    .then(string =>{
+        expect(string).toBe('aloH')
+    });
+});
+
+test('Probar Async/Await', async () =>{
+    const string = await reverseString2('Hola');
+    expect(string).toBe('aloH')
+});
+
+afterEach(() =>console.log('Después de cada prueba'));
+afterAll(() => console.log('Despues de todas las pruebas'));
+
+beforeEach(() => console.log('Antes de cada prueba'));
+beforeAll(() => console.log('Antes de todas las pruebas'));
